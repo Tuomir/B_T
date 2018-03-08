b0305da1caf272df0939beea3899b3a8a2a1e6e5
RT <tuominenri@gmail.com>
2018-03-08 21:48:56 +0200

    Moved #include order as per Visual Studio preference

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Runtime/Engine/Classes/Engine/World.h"
+
 #include "TankPlayerController.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 
 void ATankPlayerController::BeginPlay()
 {
